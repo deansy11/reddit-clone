8bd33253ef7c71e8c2f6f1cbdb0b43c4ebab2524
Emily Deans <emilyldeans@gmail.com>
2017-10-24 10:00:19 -0400

    Created list of posts and used ng-repeat directive to display all entries in the posts scope.

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ angular.module('flapperNews', [])
 .controller('MainCtrl', ['$scope',
 function($scope) {
   $scope.test = 'Hello world!';
+  // creating a new variable that defines a list of post titles
+  // scope variable is a bridge between controllers and templates - bind functions or variables you want available in template to $scope
+  $scope.posts = [
+    {title: 'post 1', upvotes: 5},
+    {title: 'post 2', upvotes: 2},
+    {title: 'post 3', upvotes: 15},
+    {title: 'post 4', upvotes: 9},
+    {title: 'post 5', upvotes: 4}
+  ];
 }]);
