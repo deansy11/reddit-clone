512805bd0e118ebf62cecf42defa49523b59b8f2
Emily Deans <emilyldeans@gmail.com>
2017-10-24 10:17:11 -0400

    Created function to push user post to posts array and button that activates function.

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 angular.module('flapperNews', [])
 .controller('MainCtrl', ['$scope',
+// scope variable is a bridge between controllers and templates - bind functions or variables you want available in template to $scope
 function($scope) {
   $scope.test = 'Hello world!';
   // creating a new variable that defines a list of post titles
-  // scope variable is a bridge between controllers and templates - bind functions or variables you want available in template to $scope
   $scope.posts = [
     {title: 'post 1', upvotes: 5},
     {title: 'post 2', upvotes: 2},
@@ -13,4 +13,8 @@ function($scope) {
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
   ];
+  // function that will add object into posts array
+  $scope.addPost = function() {
+    $scope.posts.push({title: 'A new post!', upvotes: 0});
+  };
 }]);
